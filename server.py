9cf65ab109f55164ddc8749dafc25e95ef0499e3
Claire Kim <kclaire56@yahoo.com>
2022-05-13 19:01:16 -0700

    starter code

---
+++ server.py
@@ -0,0 +1,31 @@
+from helper_functions import *
+# from flask import Flask ,render_template, session,jsonify, request ,redirect, make_response
+# from flask_debugtoolbar import DebugToolbarExtension
+from jinja2 import StrictUndefined
+from flask import Flask ,render_template,request ,redirect, make_response
+
+
+
+app = Flask('__name__')
+app.jinja_env.undefined = StrictUndefined
+app.jinja_env.auto_reload = True
+
+# Required to use Flask sessions and the debug toolbar
+app.secret_key = "nabawanda"
+
+
+#add your routes here
+@app.route('/')
+def homepage():
+
+	return render_template('homepage.html')
+
+@app.route('/calculate')
+def calculation():
+    pass
+
+	
+
+
+if __name__== "__main__":
+    app.run(debug = True)
