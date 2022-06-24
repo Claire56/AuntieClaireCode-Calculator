fdd226f3a4b91395d91c2384d7f3ef73f436bb1d
Doreen Nyakato <doreenkiryowa@gmail.com>
2022-06-25 02:51:12 +0300

    Changes to helper_functions,home.html, &server.py

--- helper_functions.py
+++ helper_functions.py
@@ -1,15 +1,26 @@
 
 
+import re
+
+
 def adding(a , b):
-    pass
+#this function sums up the two numbers
+    return a + b
 
 def subtraction(a , b):
-    pass
+#this function returns the difference between the two numbers. the position matters
+    return a - b
+
 def multiplication(a , b):
-    pass
+#this function returns the product of the two numbers
+    return a * b
 
 def modulo(a , b):
-    pass
+ #this function returns the remainder after dividing the irst number by the second number
+    return a % b
+    
 
 def power_of(a , b):
-    pass
+#this function raises the first number to the second number
+    return a ** b
+    
--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from helper_functions import *
+from helper_functions import adding,subtraction,multiplication,modulo,power_of
 # from flask import Flask ,render_template, session,jsonify, request ,redirect, make_response
 # from flask_debugtoolbar import DebugToolbarExtension
 from jinja2 import StrictUndefined
@@ -15,14 +15,40 @@ app.secret_key = "nabawanda"
 
 
 #add your routes here
-@app.route('/')
-def homepage():
+@app.route('/DoreenSimpleCalc',methods =['POST', 'GET'])
+def DoreenSimpleCalc():
+     if request.method == "POST":
+        val1 = int(request.form["num1"])
+        val2 = int(request.form["num2"])
+        operator = request.form["operator"]
 
-	return render_template('home.html')
+        return render_template('home.html')
 
 @app.route('/calculate')
 def calculation():
-    pass
+        
+   def calculation():
+        if "operator" == "add":
+            res = adding(val1, val2)
+            return render_template('home.html', val=res)
+        if "operator" == "subtract":
+            res = subtraction(val1, val2)
+            return render_template('home.html', val=res)
+        if "operator" == "multiply":
+            res = multiplication(val1, val2)
+            return render_template('home.html', val=res)
+        if "operator" == "modula":
+            res = modulo(val1, val2)
+            return render_template('home.html', val=res)
+        if "operator" == "power_of":
+            res = power_of(val1, val2)
+            return render_template('home.html', val=res)
+
+
+        else:
+
+                return render_template('home.html', val="na")
+ 
 
 	
 
